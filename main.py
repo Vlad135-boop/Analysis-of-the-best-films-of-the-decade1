31021f191ac1759a060815897e7e6a707d0fe3e8
Vlad135-boop <vlad2010sergeev@yandex.ru>
2024-04-18 13:59:54 +0300

    [ADD] test

--- main.py
+++ main.py
@@ -1,109 +1,24 @@
-from bs4 import BeautifulSoup
 import requests
-import matplotlib.pyplot as plt
-import pandas as pd
+
+from bs4 import BeautifulSoup
+from movie import Movies
 
 
-class Movies:
-    'This class was created for data analysis'
+def create_test_object():
 
-    def __init__(self, movies_soup):
-        self.movies_soup = movies_soup
+    URL_MOVIES = 'https://www.afisha.ru/selection/75-luchshih-filmov-desyatiletiya--po-versii-zapadnyh-kinokritikov/'
 
-    
-    def get_movies_list(self, tag, class_=None) -> list:
-        """
-        This function will return the top most popular movies of the decade
-
-        :param tag:
-        :param class_:
-        :return movie_list:
-        """
-
-        movie_list = []
-        movies = list(self.movies_soup.find_all(tag, class_))
-        for movie in movies:
-            movie_list.append(movie.get_text())
-        return movie_list
-
-    
-    def get_genres_stat(self, tag, class_=None) -> dict:
-        """
-        This function will return the top genres of the most popular movies
-
-        :param tag:
-        :param class_:
-        :return all_genre:
-        """
-
-        genre_list = []
-        genre_dict = {}
-        genres = list(self.movies_soup.find_all(tag, class_))
-        for genre in genres:
-            if not genre.get_text()[0].isdigit():
-                genre_list.append(genre.get_text())
-                continue
-            genre_list.append(genre.get_text()[6:])
-        
-        genre_set = set(genre_list)
-        
-        for genre in genre_set:
-            genre_dict[genre] = genre_list.count(genre)
-        all_genre = {k: v for k, v in sorted(genre_dict.items(), key=lambda item: item[1])}
-        return all_genre
+    response_films = requests.get(url=URL_MOVIES)
 
 
-    def get_rating_plot(self, tag, class_=None) -> None: 
-        """
-        This function will plot the rating of the most popular movies
-
-        :param tag:
-        :param class_:
-        :return None:
-        """
-
-        rating_list = []
-        ratings = list(self.movies_soup.find_all(tag, class_))
-        for rating in ratings:
-            rating_list.append(rating.get_text())
-        rating_list = sorted(map(float, rating_list))
-
-        df = pd.DataFrame(rating_list)
-        plt.plot(df)
-        plt.title('Рейтинги самых популярных фильмов')
-        plt.show()
+    movies_soup = BeautifulSoup(response_films.text, 'html.parser')
+    movie = Movies(movies_soup=movies_soup)
 
 
-    def get_release_years_plot(self, tag, class_=None) -> None:
-        """
-        This function will plot a bar chart in which the number of popular movies corresponds to the years of the decade
-        
-        :param tag:
-        :param class_:
-        :return None:
-        """
+    print(movie.get_movies_list('a', "CjnHd y8A5E nbCNS yknrM"))
+    print(movie.get_genres_stat('div', 'S_wwn'))
+    movie.get_rating_plot('div', 'IrSqF KDBPA BNjPz k96pX')
+    movie.get_release_years_plot('div', 'S_wwn')
 
-        year_list = []
-        year_dict = {}
-        years = list(self.movies_soup.find_all(tag, class_))
-        for year in years:
-            try:
-                year_list.append(int(year.get_text()[:4]))
-            except ValueError:
-                continue
-        
-        year_set = set(year_list)
-        for year in year_set:
-            year_dict[year] = year_list.count(year)
-
-        plt.bar(x=year_dict.keys(), height=year_dict.values())
-        plt.title('Количество лучших фильмов в каждом году десятилетия')
-        plt.show()
-    
-
-
-
-        
-
-
-
+if __name__ == '__main__':
+    create_test_object()
